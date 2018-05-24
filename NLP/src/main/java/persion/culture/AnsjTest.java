b091ca7bf6eb370154a460493275a4fd3c3ea61d
gongwenhua <gongwenhua@192.168.0.114>
2018-05-24 09:43:23 +0800

    亲

--- NLP/src/main/java/persion/culture/AnsjTest.java
+++ NLP/src/main/java/persion/culture/AnsjTest.java
@@ -16,7 +16,7 @@ import org.nlpcn.commons.lang.tire.library.Library;
 import com.huaban.analysis.jieba.JiebaSegmenter.SegMode;
 public class AnsjTest {
 	public static void test() {
-		// 我是修改后的代码111123 只关注这些词性的词 15350111 matter修改1v 分之修改了
+		// 我是修改后的代码111123 只关注这些词性的词 15350111 matter修改1v 分之修改了1
 		
 		Set<String> expectedNature = new HashSet<String>() {
 			{
